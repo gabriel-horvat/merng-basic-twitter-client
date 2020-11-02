01c155a9f799d57c5f76a68fb0679b0f6ebf830f
Gabmeister <gabriel@gabrielhorvat.com>
2020-11-02 12:22:08 +0100

    fetch posts query

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import {useQuery, gql} from '@apollo/client'
+
 
 function Home() {
+    const {loading, data} = useQuery(FETCH_POSTS_QUERY)
+
+    if (data) {
+        console.log(data);
+    }
+
     return (
         <div>
             Home page
         </div>
     )
 }
+
+const FETCH_POSTS_QUERY = gql`
+{
+    
+  getPosts {
+    id
+    body
+    createdAt
+    username
+    comments{
+      id
+      createdAt
+      body
+    }
+    likes{
+      id
+      username
+    }
+  }
+
+ 
+ }
+`
 
 export default Home
