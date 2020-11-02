cf4a213e68834bf7dff2fc9da9b95b6fba341df4
Gabmeister <gabriel@gabrielhorvat.com>
2020-11-02 19:10:31 +0100

    basic home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import {useQuery, gql} from '@apollo/client'
-
+import { Grid } from 'semantic-ui-react'
+import PostCard from '../components/PostCard'
 
 function Home() {
-    const {loading, data} = useQuery(FETCH_POSTS_QUERY)
+    const {loading, data: {getPosts: posts}} = useQuery(FETCH_POSTS_QUERY)
 
-    if (data) {
-        console.log(data);
+    if (posts) {
+        console.log(posts);
     }
 
     return (
-        <div>
-            Home page
+        
+        <Grid columns={3}>
+            <Grid.Row>
+                <h1>Posts</h1>
+            </Grid.Row>
+    <Grid.Row>
+      <Grid.Column>
+        {loading ? (
+            <h1> loading posts...</h1>
+        ): 
+        (
+            <div>
+            {posts && 
+                posts.map(post => (
+                <Grid.Column key = {post.id}>
+                    <PostCard post = {post} />
+                </Grid.Column>
+            ))}
         </div>
+        )}
+      </Grid.Column>
+    </Grid.Row>
+    </Grid>
+        
     )
 }
 
