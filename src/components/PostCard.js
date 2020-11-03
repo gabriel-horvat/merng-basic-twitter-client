afd9fd85a7ef7c4416ffa622aeec69612820fd45
Gabmeister <gabriel@gabrielhorvat.com>
2020-11-03 11:52:36 +0100

    PostCard layout

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Button, Card, Image } from 'semantic-ui-react'
+import { Icon, Button, Card, Image, Label } from 'semantic-ui-react'
 import moment from 'moment'
+import {Link } from 'react-router-dom'
+import LikeButton from './LikeButton';
 
 
 function PostCard( {
@@ -8,7 +10,7 @@ function PostCard( {
 }) {
 
 return(
-    <Card>
+    <Card fluid >
     <Card.Content>
       <Image
         floated='right'
@@ -16,20 +18,21 @@ return(
         src='https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=624&q=80'
       />
       <Card.Header>{username}</Card.Header>
-      <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+      <Card.Meta as = {Link} to = {`/posts/${id}`}>{moment(createdAt).fromNow(true)}</Card.Meta>
       <Card.Description>
 {body}
       </Card.Description>
     </Card.Content>
     <Card.Content extra>
-      <div className='ui two buttons'>
-        <Button basic color='green'>
-          Approve
-        </Button>
-        <Button basic color='red'>
-          Decline
-        </Button>
-      </div>
+    <LikeButton user="user" post={{ id, likes, likeCount }} />
+    <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
+            <Button color="blue" basic>
+              <Icon name="comments" />
+            </Button>
+            <Label basic color="blue" pointing="left">
+              {commentCount}
+            </Label>
+          </Button>
     </Card.Content>
   </Card>
 )
