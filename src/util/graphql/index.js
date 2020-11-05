402d948bbbde7fdc2f14064519b7afa4b981b613
Gabmeister <gabriel@gabrielhorvat.com>
2020-11-05 17:14:50 +0100

    refactor

--- src/util/graphql/index.js
+++ src/util/graphql/index.js
@@ -47,3 +47,15 @@ export const REGISTER_USER = gql`
     }
   }
 `;
+
+export const LOGIN_USER = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      id
+      email
+      username
+      createdAt
+      token
+    }
+  }
+`;
