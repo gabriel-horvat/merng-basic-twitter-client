16fcc5b7cd44bb5e2e60bcc4406953c0c0c2ad34
Gabmeister <gabriel@gabrielhorvat.com>
2020-11-06 12:57:35 +0100

    authRoute: don't show register if logged in

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Register from './pages/Register'
 import SinglePost from './pages/SinglePost'
 import MenuBar from './components/MenuBar';
 import {AuthProvider} from './context/auth';
+import AuthRoute from './util/AuthRoute'
 
 
 function App() {
@@ -17,8 +18,8 @@ function App() {
               <Container>
               <MenuBar />
               <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
           <Route exact path="/posts/:postId" component={SinglePost} />
     </Container>
     </Router>
