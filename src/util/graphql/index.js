3b08378f189ea7abaf4ac6aa15bfd3ba6df4c868
Gabmeister <gabriel@gabrielhorvat.com>
2020-11-06 13:12:12 +0100

    CREATE_POST_MUTATION

--- src/util/graphql/index.js
+++ src/util/graphql/index.js
@@ -59,3 +59,27 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+ export const CREATE_POST_MUTATION = gql`
+  mutation createPost($body: String!) {
+    createPost(body: $body) {
+      id
+      body
+      createdAt
+      username
+      likes {
+        id
+        username
+        createdAt
+      }
+      likeCount
+      comments {
+        id
+        body
+        username
+        createdAt
+      }
+      commentCount
+    }
+  }
+`;
